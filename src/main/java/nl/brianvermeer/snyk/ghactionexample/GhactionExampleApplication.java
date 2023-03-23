4828720a1b0a3dcc811ef807eccd3c0b3f248639
sfellows-tii <85549508+sfellows-tii@users.noreply.github.com>
2023-03-23 11:57:44 +0000

    Update GhactionExampleApplication.java

--- src/main/java/nl/brianvermeer/snyk/ghactionexample/GhactionExampleApplication.java
+++ src/main/java/nl/brianvermeer/snyk/ghactionexample/GhactionExampleApplication.java
@@ -3,6 +3,8 @@ package nl.brianvermeer.snyk.ghactionexample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+private String apiKey = "P6wa1NepBwp5wssOz9sXj7rfL3sPOvGDBdOC022CyrH5U9UtjmrDuS"; // test for secrets detection
+
 @SpringBootApplication
 public class GhactionExampleApplication {
 
