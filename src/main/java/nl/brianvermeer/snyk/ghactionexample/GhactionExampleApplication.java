916bd68e9caef97c46d6e1a6dca356389f28fae6
sfellows-tii <85549508+sfellows-tii@users.noreply.github.com>
2023-03-23 12:47:03 +0000

    Update GhactionExampleApplication.java

--- src/main/java/nl/brianvermeer/snyk/ghactionexample/GhactionExampleApplication.java
+++ src/main/java/nl/brianvermeer/snyk/ghactionexample/GhactionExampleApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 private String apiKey = "P6wa1NepBwp5wssOz9sXj7rfL3sPOvGDBdOC022CyrH5U9UtjmrDuS"; // test for secrets detection
 
+public class Constants {
+    public static final String password = "DefaultLoginPasswordDoNotChange!";
+    public static final String newKey = "mISydD0En55Fq8FXbUfX720K8Vc6/aQYtkFmkp7ntsM=";
+}
+
 @SpringBootApplication
 public class GhactionExampleApplication {
 
